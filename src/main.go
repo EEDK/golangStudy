d5d290599e63571f5dcd1587dad20f20cef5880a
EEDK <kde9604@naver.com>
2020-03-09 22:00:33 +0900

    nomadCoderGoStudy_1.0

--- src/main.go
+++ src/main.go
@@ -1,25 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-
-func fibonacci() func() int {
-	fn := 0
-	fn1 := 0
-	return func() int {
-		fn2 := fn1 + fn
-		fn = fn1
-		fn1 = fn2
-		if fn1 == 0 {fn++}
-		return fn2
-	}
-}
+import "fmt"
 
 func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
-	}
+	fmt.Println("hello world")
 }
-
