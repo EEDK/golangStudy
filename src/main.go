101266d57c390cb0e45a06df4b5bcfeeb882c0d7
EDK <kde9604@naver.com>
2020-04-06 21:25:22 +0900

    answering365Games

--- src/main.go
+++ src/main.go
@@ -39,7 +39,7 @@ func main() {
 	for url, status := range results {
 		fmt.Println(url, status)
 	}*/
-	algorithm.Convert_number_reverse(321)
+	algorithm.ThreeSixNine(100)
 }
 
 func hitURL(url string, c chan<- requestResult) {
