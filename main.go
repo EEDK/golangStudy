5e251955bd0634df23017e2d0cecaa0ee9a3d74f
EDK <kde9604@naver.com>
2020-05-02 20:25:13 +0900

    answeringQuestion34

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 /*const fileName string = "jobs.csv"
 
 func handleHome(c echo.Context) error {
@@ -21,18 +19,4 @@ func main() {
 	e.POST("/scrape", handleScrape)
 	e.Logger.Fatal(e.Start(":1323"))*/
 
-	Question33('Z')
-}
-
-func Question33(n int32){
-	if n < 'A' || n > 'Z' {
-
-	} else {
-		for c := 'A'; c <= n ; c++{
-			fmt.Printf("%2c " , c)
-		}
-		fmt.Printf("\n")
-	}
-	next := n - 1
-	Question33(next)
 }
