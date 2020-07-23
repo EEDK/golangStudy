d90791753a5ee956cd592592497d461ca813d9f3
kim dong eon <kde9604@naver.com>
2020-07-23 22:54:59 +0900

    function Add

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func lenAndUpper(name string) (length int , uppercase string) {
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-
-	return
+func superAdd(numbers ...int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
 }
 
-func main(){
-	length , _ := lenAndUpper("kimdongeon")
-	fmt.Println(length)
+func main() {
+	fmt.Println(superAdd(1, 2, 3, 4, 5, 6))
 }
-
