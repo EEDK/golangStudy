d528beaf23b3fb5e7b305c87dddd37c9fe3e8617
kim dong eon <kde9604@naver.com>
2020-07-25 15:12:21 +0900

    Go Lang's Map

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{"kim", "dong","eon"}
-	names = append(names, "flynn")
-
-	fmt.Println(names)
+	nico := map[string]string{"name": "nico"}
+	fmt.Println(nico)
 }
