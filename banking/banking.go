b153c0bffae69d701438a428f261462dce11e73d
kim dong eon <kde9604@naver.com>
2020-07-26 21:31:12 +0900

    method

--- banking/banking.go
+++ banking/banking.go
@@ -11,3 +11,12 @@ func NewAccount(owner string) *BankAccount {
 	account := BankAccount{owner: owner , balance: 0}
 	return &account
 }
+
+func (a BankAccount) Balance() int {
+	return a.balance
+}
+
+// Get Actual account Change 
+func (a *BankAccount) Deposit(amount int){
+	a.balance += amount
+}
--- main.go
+++ main.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	"fmt"
+	"golangStudy/banking"
 )
 
 func main() {
-	m := make(map[string]int)
-
-	m["Answer"] = 42
-	fmt.Println("The value:", m["Answer"])
-
-	m["Answer"] = 48
-	fmt.Println("The value:", m["Answer"])
-
-	delete(m, "Answer")
-	fmt.Println("The value:", m["Answer"])
-	
-
-	v, ok := m["Answer"]
-	fmt.Println("The value:", v, "Present?", ok)
+	account := banking.NewAccount("kde")
+	account.Deposit(10)
+	fmt.Println(account.Balance())
 }
