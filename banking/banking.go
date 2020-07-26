f8ba7d459154652d3150650b38a793c14c69d144
kim dong eon <kde9604@naver.com>
2020-07-26 22:47:52 +0900

    bank done

--- banking/banking.go
+++ banking/banking.go
@@ -1,6 +1,11 @@
 package banking
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
+
+var errNoMoney = errors.New("Your account have few money to handle this activity")
 
 // BankAccount struct
 type BankAccount struct{
@@ -19,15 +24,29 @@ func (a BankAccount) Balance() int {
 	return a.balance
 }
 
-func (a *BankAccount) Deposit(amount int){
+func (a BankAccount) Owner() string{
+	return a.owner
+}
+
+
+// Activity
+func (a *BankAccount) AccountDeposit(amount int){
 	a.balance += amount
 }
 
-func (a *BankAccount) Withdraw(amount int) (error) {
+func (a BankAccount) String() string{
+	return fmt.Sprint(a.Owner(), "'s account.\nHas ", a.Balance())
+}
+
+func (a *BankAccount) AccountWithdraw(amount int) error {
 	if a.balance < amount {
-		return errors.New("Can't withdraw your account")
+		return errNoMoney
 	}
 
 	a.balance -= amount
 	return nil
+}
+
+func (a *BankAccount) ChangeAccountOwner(newOwner string)  {
+	a.owner = newOwner
 }
--- main.go
+++ main.go
@@ -3,16 +3,11 @@ package main
 import (
 	"fmt"
 	"golangStudy/banking"
-	"log"
 )
 
 func main() {
 	account := banking.NewAccount("kde")
-	account.Deposit(10)
-	fmt.Println(account.Balance())
-
-	err := account.Withdraw(20)
-	if err != nil{
-		log.Fatalln(err)
-	}
+	account.AccountDeposit(10)
+	account.ChangeAccountOwner("Juwon")
+	fmt.Println(account)
 }
